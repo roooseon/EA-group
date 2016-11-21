40352cafcd71a75599ef30cc5827d99d0bf1c69f
RojanPiya <piya_roj@hotmail.com>
2016-11-20 22:48:52 -0600

    removed user

---
+++ src/main/java/cs544/carrental/domain/Admin.java
@@ -0,0 +1,47 @@
+package cs544.carrental.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Admin {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String userName;
+	private String password;
+	
+	@OneToMany (mappedBy="admin")
+	private List<Car> cars;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<Car> getCars() {
+		return cars;
+	}
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
+	
+}
